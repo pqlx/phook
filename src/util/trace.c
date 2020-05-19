e939e12108054a1928577686f9e3ff84852c5e9c
David <bpf.parser@gmail.com>
2020-05-19 11:41:54 +0200

    fix stupid typos&bug

--- src/util/trace.c
+++ src/util/trace.c
@@ -6,7 +6,7 @@ uint64_t ptrace_write_u32(pid_t pid, void* addr, uint32_t value)
 {
     uint64_t data, new; 
     
-    data = new = ptrace(PTRACE_PEEKDATA, pid, addr, NONE);
+    data = new = ptrace(PTRACE_PEEKDATA, pid, addr, NULL);
     new  &= 0xffffffff00000000;
     new  |= value;
     ptrace(PTRACE_POKEDATA, pid, addr, new);
@@ -18,7 +18,7 @@ uint64_t ptrace_write_u16(pid_t pid, void* addr, uint16_t value)
 {
     uint64_t data, new;
     
-    data = new = ptrace(PTRACE_PEEKDATA, pid, addr, NONE);
+    data = new = ptrace(PTRACE_PEEKDATA, pid, addr, NULL);
     new &= 0xffffffffffff0000;
     new |= value;
     ptrace(PTRACE_POKEDATA, pid, addr, new);
@@ -30,7 +30,7 @@ uint64_t ptrace_write_u8(pid_t pid, void* addr, uint8_t value)
 {
     uint64_t data, new;
 
-    data = new = ptrace(PTRACE_PEEKDATA, pid, addr, NONE);
+    data = new = ptrace(PTRACE_PEEKDATA, pid, addr, NULL);
     new &= 0xffffffffffffff00;
     new |= value;
     ptrace(PTRACE_POKEDATA, pid, addr, new);
--- src/util/trace.h
+++ src/util/trace.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdint.h>
 
 /* These helper functions return the original 8-byte value at the addr,
  * as to not completely waste the PEEKDATA call in some scenarios */
-uint64_t ptrace_write_u32(pid_t, void*, int32_t);
+uint64_t ptrace_write_u32(pid_t, void*, uint32_t);
 uint64_t ptrace_write_u16(pid_t, void*, uint16_t);
 uint64_t ptrace_write_u8(pid_t,  void*, uint8_t);

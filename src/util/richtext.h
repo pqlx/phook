d1e78242d227fb7c60a173001d67d1894df177dd
David <bpf.parser@gmail.com>
2020-05-20 05:05:34 +0200

    more colors

--- src/util/richtext.h
+++ src/util/richtext.h
@@ -1,5 +1,11 @@
 #pragma once
 
-#define TERM_COLOR_RED "\x1b[31m"
-#define TERM_COLOR_GREEN "\x1b[32m"
+#define TERM_COLOR_BLACK   "\x1b[30m"
+#define TERM_COLOR_RED     "\x1b[31m"
+#define TERM_COLOR_GREEN   "\x1b[32m"
+#define TERM_COLOR_YELLOW  "\x1b[33m"
+#define TERM_COLOR_BLUE    "\x1b[34m"
+#define TERM_COLOR_MAGENTA "\x1b[35m"
+#define TERM_COLOR_CYAN    "\x1b[36m"
+#define TERM_COLOR_WHITE   "\x1b[37m"
 #define TERM_RESET "\x1b[0m"

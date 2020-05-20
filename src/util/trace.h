36ebe0a5a6acbebf6a6869ba1d8ebe5bc8b4fe23
David <bpf.parser@gmail.com>
2020-05-20 08:25:16 +0200

    fix bug and properly multiple offset by 8

--- src/util/trace.h
+++ src/util/trace.h
@@ -31,8 +31,8 @@ uint64_t ptrace_read_write_u32(pid_t, void*, uint32_t);
 uint64_t ptrace_read_write_u16(pid_t, void*, uint16_t);
 uint64_t ptrace_read_write_u8 (pid_t,  void*, uint8_t);
 
-#define ptrace_set_reg_u64(pid, offset, value) ptrace(PTRACE_POKEUSER, (pid), (offset), (value))
-#define ptrace_get_reg_u64(pid, offset) ptrace(PTRACE_PEEKUSER, (pid), (offset), NULL)
+#define ptrace_set_reg_u64(pid, offset, value) ptrace(PTRACE_POKEUSER, (pid), (8*offset), (value))
+#define ptrace_get_reg_u64(pid, offset) ptrace(PTRACE_PEEKUSER, (pid), (8*offset), NULL)
 
 uint64_t ptrace_get_set_reg_u64(pid_t, uint64_t, uint64_t);
 uint64_t ptrace_get_set_reg_u32(pid_t, uint64_t, uint32_t);
